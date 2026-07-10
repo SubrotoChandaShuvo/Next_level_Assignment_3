d33374d393020332cf72386b37c0d09dc0c73659
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2026-07-10 14:00:19 +0600

    database Football_Ticket_Booking_System is created

---
+++ createTable.sql
@@ -0,0 +1,7 @@
+create database Football_Ticket_Booking_System;
+
+
+-- DROP TABLES IF THEY ALREADY EXIST TO PREVENT CONFLICTS (In my dataBase there is no Bookings, Matches, Users table so I needn't drop any table. I just keep it from the questions)
+DROP TABLE IF EXISTS Bookings;
+DROP TABLE IF EXISTS Matches;
+DROP TABLE IF EXISTS Users;
