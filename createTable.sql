fccd9e438bb145adad07122217787a4367946132
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2026-07-10 14:09:48 +0600

    CREATE USERS TABLE

--- createTable.sql
+++ createTable.sql
@@ -5,3 +5,14 @@ create database Football_Ticket_Booking_System;
 DROP TABLE IF EXISTS Bookings;
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Users;
+
+
+-- 1. CREATE USERS TABLE
+CREATE TABLE Users (
+    user_id int primary key,
+    full_name varchar(100) not null,
+    email varchar(100) unique not null,
+    role varchar(20),
+    phone_number varchar(20),
+    check (role in ('Football Fan', 'Ticket Manager'))
+);
